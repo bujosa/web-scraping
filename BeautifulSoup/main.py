a3cdff8d0bc7083863d791abe15baf889cc42352
bujosa <davidbujosa@gmail.com>
2021-04-12 02:39:21 -0400

    Added new main

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -1,1 +1,10 @@
 from bs4 import BeautifulSoup
+import requests
+
+# Request to azul
+response = requests.get("")
+
+azul = response.text
+soup = BeautifulSoup(azul, "html.parser")
+print(soup.title)
+# print(soup.prettify())
