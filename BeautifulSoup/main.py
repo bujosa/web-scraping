6d93479c3613638c1cae48cb47f6e18a6f68017a
bujosa <davidbujosa@gmail.com>
2021-04-13 02:38:41 -0400

    Added get specefic brand page

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pymongo
 import dns
+import math
 
 # Database Name and db connection string to mongo atlas
 dbName = 'MercadoLibreMX'
@@ -15,6 +16,7 @@ mercadoLibre = response.text
 soup = BeautifulSoup(mercadoLibre, "html.parser")
 
 max_vehicle_per_page = 48
+limit_car_per_brand = 1969
 
 # Get all the brand and find your url
 def get_brand_url(soup):
@@ -24,12 +26,17 @@ def get_brand_url(soup):
         key = brand.get("href")
         value_tmp = brand.find("span", class_="ui-search-search-modal-filter-match-count").text
         value = int(value_tmp.replace("(","").replace(")","").replace(",",""))
+        if value > limit_car_per_brand:
+            value = limit_car_per_brand
         brand_href[key] = value
 
     return brand_href
 
 def get_car_url(key, value):
-    print(key)
+    url = convert_url(key)
+    brand_specific_urls = get_array_of_url(url, value)
+    for specific_page in brand_specific_urls:
+        print(specific_page)
     return None
 
 def get_car_information(soup):
@@ -37,7 +44,25 @@ def get_car_information(soup):
 
 # Transform url
 def convert_url(url):
-    return None
+    result = url.split("?")
+    return result[0]
+
+def get_array_of_url(url, value):
+    brand_url = []
+    last_part = "_Desde_"
+    brand_url.append(url)
+    count = value/max_vehicle_per_page
+
+    if count < 1 or value == max_vehicle_per_page: 
+        return brand_url
+    else:
+        count = math.floor(count)
+        for x in range(count+1):
+            number = str(x*max_vehicle_per_page + 1)
+            last_part_tmp = url+last_part+number
+            brand_url.append(last_part_tmp)
+    
+    return brand_url
 
 #Vehicle data manager
 class VehicleDataManager():
