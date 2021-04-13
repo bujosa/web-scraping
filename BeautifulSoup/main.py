392bb0277aabc18b9d5da6efcdfc2021537f5b4a
bujosa <davidbujosa@gmail.com>
2021-04-13 03:38:57 -0400

    Fixed get model

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -18,6 +18,9 @@ soup = BeautifulSoup(mercadoLibre, "html.parser")
 max_vehicle_per_page = 48
 limit_car_per_brand = 1969
 
+#Fields
+fields = { "year":"Año", "fuelType": "Tipo de combustible", "transmission": "Transmisión", "bodyStyle": "Tipo de carrocería",  "doors":"Puertas",  "engine": "Motor",  "mileage": "Kilómetros"}
+
 # Get all the brand and find your url
 def get_brand_url(soup):
     brand_href = {}
@@ -35,12 +38,68 @@ def get_brand_url(soup):
 def get_car_url(key, value):
     url = convert_url(key)
     brand_specific_urls = get_array_of_url(url, value)
+
     for specific_page in brand_specific_urls:
-        print(specific_page)
-    return None
+        response = requests.get(specific_page)
+        car_page = response.text
+        soup = BeautifulSoup(car_page, "html.parser")
+        get_car_information(soup)
+
+def price_section(soup):
+    price_section = soup.find("span", class_="price-tag-fraction")
+    price = int(price_section.text.replace(",",""))
+    return price
+
+# extract data on data_sheet
+def data_sheet(soup):
+    data = {}
+    columns = soup.find("tbody", class_="andes-table__body").find_all("tr")
+
+    for row in columns:
+        key = row.find("th").text
+        value = row.find("td").text
+        data[key] = value
+    
+    return data
+
+# transform and get model
+def get_model(dict, title, brand):
+  model = title.replace(brand, "")
+  for key in dict:
+      if key == "Transmisión" or key == "Puertas":
+          continue
+      model = model.replace(dict[key], "")
+  return model
 
 def get_car_information(soup):
-    return None
+    picture_section = soup.find("img", class_="ui-pdp-image ui-pdp-gallery__figure__image")
+
+    price = price_section(soup)
+    title = picture_section.get("alt")
+    brand = title.split(" ")[0]
+    mainPicture = picture_section.get("data-zoom")
+    
+    data_sheet_table = data_sheet(soup)
+    
+    model = get_model(data_sheet_table, title, brand)
+
+    vehicle = {
+       "title":title, 
+       "brand": brand,
+       "model": model,
+       "price": price, 
+       "mainPicture": mainPicture,
+       "year": data_sheet_table[fields["year"]],
+       "fuelType": data_sheet_table[fields["fuelType"]],
+       "bodyStyle": data_sheet_table[fields["bodyStyle"]],
+       "transmission": data_sheet_table[fields["transmission"]],
+       "engine": data_sheet_table[fields["engine"]],
+       "doors": data_sheet_table[fields["doors"]],
+       "mileage": data_sheet_table[fields["mileage"]],
+    }
+    
+    print(vehicle)
+    return vehicle
 
 # Transform url
 def convert_url(url):
@@ -78,7 +137,15 @@ class VehicleDataManager():
         vehicleObject = self.vehicleDataExtractor(vehiclePage)
         self.collection.insert_one(vehicleObject)
 
-brand_url_and_count = get_brand_url(soup)
+# brand_url_and_count = get_brand_url(soup)
 
-for key in brand_url_and_count:
-    get_car_url(key, brand_url_and_count[key])
+# for key in brand_url_and_count:
+#     get_car_url(key, brand_url_and_count[key])
+
+response = requests.get("https://auto.mercadolibre.com.mx/MLM-891858876-mazda-cx-3-20-i-grand-touring-at-2016-_JM#position=47&type=item&tracking_id=bde0cb13-a342-4928-8705-6554ade7aa0f")
+
+mercadoLibre = response.text
+
+soup = BeautifulSoup(mercadoLibre, "html.parser")
+
+get_car_information(soup)
