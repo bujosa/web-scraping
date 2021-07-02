c274797c395be116e30f6ccde5f746fb7756bef1
bujosa <davidbujosa@gmail.com>
2021-07-02 12:21:57 -0400

    Added new function

--- MercadoLibreRD/main.py
+++ MercadoLibreRD/main.py
@@ -41,6 +41,44 @@ def get_year_url(soup):
 def convert_url(url):
     result = url.split("?")
     return result[0]
+
+def get_car_url(key, value):
+    brand_specific_urls = get_array_of_url(key, value)
+
+    for specific_page in brand_specific_urls:
+        response = requests.get(specific_page)
+        car_page = response.text
+        soup = BeautifulSoup(car_page, "html.parser")
+
+        validator = soup.find("svg", class_="ui-search-icon ui-search-icon--not-found ui-search-rescue__icon")
+        
+        if validator != None:
+            break
+
+        urls = soup.find("section", class_="ui-search-results ui-search-results--without-disclaimer").find_all("li", class_="ui-search-layout__item")
+
+        for url in urls:
+            car_url = url.find("a", class_="ui-search-result__content ui-search-link").get("href")
+            get_car_information(car_url)
+
+def get_array_of_url(url, value):
+    brand_url = []
+    last_part = "_Desde_"
+    brand_url.append(url)
+    count = value/max_vehicle_per_page
+
+    if count < 1 or value == max_vehicle_per_page: 
+        return brand_url
+    else:
+        count = math.floor(count)
+        for x in range(count+1):
+            number = str(x*max_vehicle_per_page + 1)
+            last_part_tmp = url+last_part+number
+            brand_url.append(last_part_tmp)
+    
+    return brand_url
+
+
 #Vehicle data manager
 class VehicleDataManager():
     def __init__(self): 
@@ -56,5 +94,5 @@ class VehicleDataManager():
 
 year_url_and_count = get_year_url(soup)
 
-for key in brand_url_and_count:
-    get_car_url(key, brand_url_and_count[key])
+for key in year_url_and_count:
+    get_car_url(key, year_url_and_count[key])
