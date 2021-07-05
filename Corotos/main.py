4535d621393b863f5099d55492535cd83f6f8edd
bujosa <davidbujosa@gmail.com>
2021-07-05 02:34:13 -0400

    Finished corotos class

--- Corotos/main.py
+++ Corotos/main.py
@@ -4,145 +4,124 @@ import pymongo
 import dns
 import math
 
-# Database Name and db connection string to mongo atlas
-dbName = 'Corotos'
-dbConnectionString = "YOUR_DATA_BASE_URL"
+class Corotos:
+    __fields = { "year":"Año", "brand": "Marca", "model": "Modelo",
+    "fuelType": "Combustible", "transmission": "Transmisión",
+    "bodyStyle": "Tipo",  "doors":"Cantidad de puertas",  "engine": "Motor/CC",
+    "mileage": "Kilometraje", "colorExterior": "Color exterior",
+    "colorInterior": "Color interior",
+    "driveTrain": "Tracción",
+    "US$": "USD" , "RD$": "DOP"}
+     
+    def __data_sheet(self, soup):
+        data = {}
+        try: 
+            columns = soup.find("ul", class_="post__specs").find_all("li")
+        except: 
+            return data
 
-# Request to Corotos
-response = requests.get("https://www.corotos.com.do/listings/mercedes-benz-e350-2014-avantgarde-01f8xrswtx6m2rd4f73cdse8h1?page=4&q%5Bcategory_slug_eq%5D=veh%C3%ADculos&render_time=2021-07-04T21%3A52%3A57.402212-04%3A00")
-
-corotos = response.text
-
-soup = BeautifulSoup(corotos, "html.parser")
-
-#Fields
-fields = { "year":"Año", "brand": "Marca", "model": "Modelo", "fuelType": "Tipo de combustible", "transmission": "Transmisión", "bodyStyle": "Tipo de carrocería",  "doors":"Puertas",  "engine": "Motor",  "mileage": "Kilómetros", "color": "Color", "dólares": "USD" , "pesos": "DOP"}
-
-def data_sheet(soup):
-    data = {}
-    try: 
-        columns = soup.find("tbody", class_="andes-table__body").find_all("tr")
-    except: 
+        for row in columns:
+            key = row.find("span", class_="specs__label").text
+            value = row.find("span", class_="specs__value").text
+            data[key] = value
+        
         return data
 
-    for row in columns:
-        key = row.find("th").text
-        value = row.find("td").text
-        data[key] = value
+    def __get_key(self, str):
+        return self.__fields[str]
+
+    def __get_price_and_currency(self, str):
+        array = str.split(' ')
+        currency = self.__fields[array[0]]
+        price = int(array[1].replace(",", ""))
+        return price, currency
+
+    def __get_main_picture(self, soup):
+        try:
+            carousel = soup.find("div", class_="carousel-cell")
+            return carousel.find("img").get("src")
+        except:
+            return None
+
+    def __get_date(self, str):
+        date = str.replace("Publicado: ", "").replace(" de", "")
+        return date
     
-    return data
+    def __key_error(self, data, key):
+        try:
+            return data[self.__fields[key]]
+        except:
+            return None
 
-def get_key(key):
-    return fields[key]
+    def __header_section(self, soup):
+        header = dict()
 
-def key_error(data, key):
-    try:
-        if key == "year" or key == "mileage":
-            return int(data[fields[key]].replace(" km",""))
-        else:
-            return data[fields[key]]
-    except:
-        return None
+        post_details = soup.find("div", class_="post__details")
 
-def price_section(soup):
-    price_section = soup.find("span", class_="price-tag-text-sr-only")
+        if post_details == None:
+            return None
 
-    if price_section == None:
-        return None
-    
-    keys = price_section.text.split(" ")
-    
-    price = int(keys[0])
-    currency = get_key(keys[1])
-    
-    if price > 200 and price < 999:
-        return price*1000, "DOP"
+        post_price = post_details.find("h2", class_="post__price")
 
-    if price < 2000: 
-        return None, None
+        post_title = post_details.find("h1", class_="post__title")
 
-    if price < 100000 and currency == "DOP": 
-        currency =  "USD"
+        post_date = post_details.find("p", class_="post__date")
 
-    return price, currency
+        if post_price == None or post_title == None or post_date == None:
+            return None
 
-def get_car_information(url):
-    response = requests.get(url)
-    vehicle_detail_page = response.text
-    soup = BeautifulSoup(vehicle_detail_page, "html.parser")
+        header["title"] = post_title.text
 
-    picture_section = soup.find("img", class_="ui-pdp-image ui-pdp-gallery__figure__image")
+        price, currency =  self.__get_price_and_currency(post_price.text)
+        
+        header["price"] = price
+        header["currency"] = currency
 
-    price, currency = price_section(soup)
+        header["date"] =  self.__get_date(post_date.text)
 
-    if picture_section == None:
-        return
+        return header
 
-    originalMainPicture = picture_section.get("data-zoom")
+    def get_car_information(self, url):
+        response = requests.get(url)
+        vehicle_detail_page = response.text
+        soup = BeautifulSoup(vehicle_detail_page, "html.parser")
 
-    title = picture_section.get("alt")
+        originalMainPicture = self.__get_main_picture(soup)
 
-    if title == None:
-        return
+        header = self.__header_section(soup)
 
-    brand = title.split(" ")[0]
+        if header == None:
+            return 
 
-    days = days_section(soup)
+        data_sheet_table = self.__data_sheet(soup)
 
-    if days > 60 :
-      return
+        if data_sheet_table == {}:
+            return
 
-    data_sheet_table = data_sheet(soup)
+        vehicle = {
+            "title":header["title"], 
+            "brand": self.__key_error(data_sheet_table, "brand"),
+            "model": self.__key_error(data_sheet_table, "model"),
+            "price": header["price"], 
+            "currency": header["currency"],
+            "date": header["date"],
+            "mainPicture": "https://curbo-assets.nyc3.cdn.digitaloceanspaces.com/Curbo%20proximamente.svg",
+            "originalMainPicture": originalMainPicture,
+            "year": self.__key_error(data_sheet_table, "year"),
+            "fuelType": self.__key_error(data_sheet_table, "fuelType"),
+            "bodyStyle": self.__key_error(data_sheet_table, "bodyStyle"),
+            "driveTrain": self.__key_error(data_sheet_table, "driveTrain"),
+            "transmission": self.__key_error(data_sheet_table, "transmission"),
+            "engine": self.__key_error(data_sheet_table, "engine"),
+            "doors": self.__key_error(data_sheet_table, "doors"),
+            "mileage": self.__key_error(data_sheet_table, "mileage"),
+            "colorExterior": self.__key_error(data_sheet_table, "colorExterior"),
+            "colorInterior": self.__key_error(data_sheet_table, "colorInterior"),
+            "vehicle_url": url,
+        }
 
-    if data_sheet_table == {}:
-        return
-    
-    model = get_model(data_sheet_table, title, brand)
+        return vehicle
 
-    if price == None:
-        return
-   
-    if key_error(data_sheet_table, "brand") != None:
-        brand = key_error(data_sheet_table, "brand")
-     
-    if key_error(data_sheet_table, "model") != None:
-        model = key_error(data_sheet_table, "model")
+vehicle = Corotos()
 
-    vehicle = {
-       "title":title, 
-       "brand": brand,
-       "model": model,
-       "price": price, 
-       "currency": currency,
-       "age": days,
-       "mainPicture": "https://curbo-assets.nyc3.cdn.digitaloceanspaces.com/Curbo%20proximamente.svg",
-       "originalMainPicture": originalMainPicture,
-       "year": key_error(data_sheet_table, "year"),
-       "fuelType": key_error(data_sheet_table, "fuelType"),
-       "bodyStyle": key_error(data_sheet_table, "bodyStyle"),
-       "transmission": key_error(data_sheet_table, "transmission"),
-       "engine": key_error(data_sheet_table, "engine"),
-       "doors": key_error(data_sheet_table, "doors"),
-       "mileage": key_error(data_sheet_table, "mileage"),
-       "color": key_error(data_sheet_table, "color"),
-       "vehicle_url": url,
-    }
-
-    global count
-    count += 1
-    print(count)
-
-    VehicleDataManager().addCar(vehicle)
-
-#Vehicle data manager
-class VehicleDataManager():
-    def __init__(self): 
-            self.connection = pymongo.MongoClient(
-                dbConnectionString
-            )
-
-            db = self.connection[dbName]
-            self.collection = db['Cars']
-
-    def addCar(self, vehicleObject):
-        self.collection.insert_one(vehicleObject)
+print(vehicle.get_car_information("https://www.corotos.com.do/listings/chevrolet-camaro-ss-2016-01f9tmddfe71fk4xc9skbj7t62?page=1&q%5Bcategory_slug_eq%5D=veh%C3%ADculos&render_time=2021-07-05T02%3A11%3A08.918255-04%3A00"))
