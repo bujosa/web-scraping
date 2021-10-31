40b41e2a60c1579f8520a99e56716a10a858b9a3
bujosa <davidbujosa@gmail.com>
2021-10-31 22:16:19 +0100

    Add get seller and get sellet type on mercado libre rd

--- MercadoLibreRD/main.py
+++ MercadoLibreRD/main.py
@@ -171,6 +171,20 @@ def get_car_url(key, value):
 def get_key(key):
     return fields[key]
 
+def get_seller(soup):
+    try:
+        seller = soup.find("h3", class_="ui-pdp-color--BLACK ui-pdp-size--LARGE ui-pdp-family--REGULAR").text
+        return seller
+    except:
+        return ''
+
+def get_seller_type(soup):
+    try:
+        sellerType = soup.find("p", class_="ui-pdp-color--GRAY ui-pdp-family--REGULAR ui-vip-profile-info__subtitle").text
+        return sellerType
+    except:
+        return 'Particular'
+
 def get_model(dict, title, brand):
   if title == None:
     return None
